728ee167a84c6ce01e09c12bb0a5c2e5e1c3a06b
Theo Collignon <tcollign@utbm.fr>
2019-09-27 12:04:29 +0200

    test

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,2 +1,3 @@
 class Student < ApplicationRecord
+    validates :name, :last_name, presence: true
 end
--- test/models/student_test.rb
+++ test/models/student_test.rb
@@ -1,7 +1,14 @@
 require 'test_helper'
 
 class StudentTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  test "should have the necessary required validators" do
+    student = Student.new
+    assert_not student.valid?
+    assert_equal [:name, :last_name], student.errors.keys
+
+    student.name = "Gérard"
+    student.last_name = "Bouchard"
+    assert student.valid?
+  end
 end

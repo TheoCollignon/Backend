df09934dc2458870fa550ca49d6b66a02b200cd8
TheoCollignon <theo.collignon2506@gmail.com>
2019-10-04 15:25:59 +0200

    counter websocket

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   resources :counters
   resources :students
+
+  mount ActionCable.server => '/cable'
+  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
